cffec2f9abe83ea3b8f59170159e5dd2230b5cf9
AndersonPaulkirubakaran <40992295+AndersonPaulkirubakaran@users.noreply.github.com>
2018-07-11 11:16:25 +0530

    Create natural num.py

---
+++ natural num.py
@@ -0,0 +1,13 @@
+number = 16
+
+
+
+if number < 0:
+	print("Enter a positive number")
+else:
+   sum = 0
+  
+   while(number > 0):
+       sum += number
+       number -= 1
+	print("The sum is",sum)
